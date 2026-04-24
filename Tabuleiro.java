8130d77531fcc952791cffae2133eb2a2b669bcb
manu0312090-hub <manu0312090@gmail.com>
2026-04-23 23:10:30 -0300

    Tabuleiro.java

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -6,20 +6,20 @@ public class Tabuleiro {
     public Tabuleiro() {
         grade = new char[3][3];
         for (int i = 0; i < 3; i++) {
-            Arrays.fill(grade[i], ' ');
+            Arrays.fill(grade[i], ' '); 
         }
     }
 
     public void exibir() {
-        System.out.println("\n  0 1 2");
+        System.out.println("\n   0 1 2");
         for (int i = 0; i < 3; i++) {
-            System.out.print(i + " ");
+            System.out.print(i + "  ");
             for (int j = 0; j < 3; j++) {
                 System.out.print(grade[i][j]);
                 if (j < 2) System.out.print("|");
             }
             System.out.println();
-            if (i < 2) System.out.println("  -----");
+            if (i < 2) System.out.println("   -----");
         }
     }
 
@@ -34,7 +34,9 @@ public class Tabuleiro {
     public boolean verificarVencedor(char s) {
         for (int i = 0; i < 3; i++) {
             if ((grade[i][0] == s && grade[i][1] == s && grade[i][2] == s) ||
-                (grade[0][i] == s && grade[1][i] == s && grade[2][i] == s)) return true;
+                (grade[0][i] == s && grade[1][i] == s && grade[2][i] == s)) {
+                return true;
+            }
         }
         return (grade[0][0] == s && grade[1][1] == s && grade[2][2] == s) ||
                (grade[0][2] == s && grade[1][1] == s && grade[2][0] == s);
@@ -43,7 +45,9 @@ public class Tabuleiro {
     public boolean estaCheio() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (grade[i][j] == ' ') return false;
+                if (grade[i][j] == ' ') {
+                    return false;
+                }
             }
         }
         return true;
